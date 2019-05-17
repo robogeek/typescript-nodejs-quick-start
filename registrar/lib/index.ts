c3a9b07a71938e2494c2a1861616d53e2c141750
David Herron <david@davidherron.com>
2019-05-16 23:29:59 -0700

    Update RegistrarDB module for TypeORM

--- registrar/lib/index.ts
+++ registrar/lib/index.ts
@@ -1,25 +1,77 @@
+import "reflect-metadata";
+import { createConnection, getManager, getRepository, getConnection } from "typeorm";
 
-import * as ForerunnerDB from 'forerunnerdb';
+import { isGender, isStudent, isStudentUpdater } from './types';
+import { Student } from './entities/Student';
+import { OfferedClass } from './entities/Class';
+
 import * as path from 'path';
-import { StudentDB } from './Student';
-export { Student } from './Student';
+import * as util from 'util';
 
 export default class RegistrarDB {
-    private _fdb : ForerunnerDB;
-    private _db;
-    private _students;
 
-    constructor(dbpath: string) {
-        this._fdb = new ForerunnerDB();
-        this._db = this._fdb.db("test");
-        this._students = new StudentDB(this, "students");
-        this._db.persist.dataDir(dbpath);
-        this._db.load();
-        console.log(`RegistrarDB dbpath ${dbpath}`);
+    constructor() {
     }
 
-    get db() { return this._db; }
-    get fdb() { return this._fdb; }
-    get students() { return this._students; }
+    async init(databaseFN: string) {
+        await createConnection({
+            type: "sqlite",
+            database: databaseFN,
+            synchronize: true,
+            logging: false,
+            entities: [
+                Student, OfferedClass
+            ]
+         });
+    }
+
+    async close() {
+        let conn = getConnection();
+        if (conn) await conn.close();
+    }
+
+    async drop() {
+        let conn = getConnection();
+        await conn.dropDatabase();
+    }
+
+    get studentRepository() { return getRepository(Student); }
+
+    async createStudent(student: Student): Promise<any> {
+        if (!isStudent(student)) {
+            throw new Error(`Supplied student object not a Student ${util.inspect(student)}`);
+        }
+        let stud = new Student();
+        stud.name = student.name;
+        stud.entered = student.entered;
+        stud.grade = student.grade;
+        stud.gender = student.gender;
+        await this.studentRepository.save(stud);
+        return stud.id;
+    }
+
+    async getStudent(id: any): Promise<Student> {
+        let student = await this.studentRepository.findOne({ where: { id: id }});
+        if (!isStudent(student)) {
+            throw new Error(`Student id ${util.inspect(id)} did not retrieve a Student`);
+        }
+        return student;
+    }
+
+    async updateStudent(id: any, student: Student): Promise<any> {
+        if (!isStudentUpdater(student)) {
+            throw new Error(`Student update id ${util.inspect(id)} did not receive a Student updater ${util.inspect(student)}`);
+        }
+        await getManager().update(Student, id, student);
+    }
+    
+    async deleteStudent(student: number | Student) {
+        if (typeof student !== 'number' && !isStudent(student)) {
+            throw new Error('Supplied student object not a Student');
+        }
+        // console.log(`deleteStudent ${typeof student === 'number' ? student : student.id}`);
+        await getManager().delete(Student, typeof student === 'number' ? student : student.id);
+    }
+
 }
 
--- registrar/test/index.js
+++ registrar/test/index.js
@@ -2,21 +2,29 @@
 const util = require('util');
 const path = require('path');
 const assert = require('chai').assert;
-const RegistrarDB = require('../dist/index').RegistrarDB;
+const RegistrarDB = require('../dist/index').default;
+const { Student } = require('../dist/entities/Student');
+const { getManager, getRepository } = require("typeorm");
+
+var registrar;
 
 describe('Initialize Registrar', function() {
-    it('should successfully initialize the Registrar', function() {
-        let registrar = new RegistrarDB(path.join(__dirname, 'testDB'));
+    before(async function() {
+        registrar = new RegistrarDB();
+        await registrar.init("registrardb.sqlite");
+    });
+
+    it('should successfully initialize the Registrar', async function() {
 
         assert.exists(registrar);
         assert.isObject(registrar);
-        assert.isObject(registrar.db);
-        assert.isObject(registrar.students);
+        // assert.isObject(registrar.db);
+        // assert.isObject(registrar.students);
     });
 });
 
-describe('Add students to empty registry', function() {
-    let registrar;
+describe('Add students to registry', function() {
+    // let registrar;
     let stud1 = {
         name: "John Brown", 
         entered: 1997, grade: 4,
@@ -30,15 +38,11 @@ describe('Add students to empty registry', function() {
     let studentid1;
     let studentid2;
 
-    before(function() {
-        registrar = new RegistrarDB(path.join(__dirname, 'testDB'));
-    });
-
     it('should add a student to the registry', async function() {
-        studentid1 = await registrar.students.addStudent(stud1);
+        studentid1 = await registrar.createStudent(stud1);
         // console.log(`should add got studentid1 ${util.inspect(studentid1)}`);
-        let student = await registrar.students.student(studentid1);
-        console.log(`should add got studentid1 ${util.inspect(studentid1)} student ${util.inspect(student)}`);
+        let student = await registrar.getStudent(studentid1);
+        // console.log(`should add got studentid1 ${util.inspect(studentid1)} student ${util.inspect(student)}`);
         assert.exists(student);
         assert.isObject(student);
         assert.isString(student.name);
@@ -54,11 +58,109 @@ describe('Add students to empty registry', function() {
     it('should fail to add a student with bad data', async function() {
         let sawError = false;
         try {
-            await registrar.students.addStudent(stud2);
+            await registrar.createStudent(stud2);
         } catch (err) {
-            // console.log(`should fail caught ${err.stack}`);
+            // console.log(`should fail caught ${err.message}`);
             sawError = true;
         }
         assert.isTrue(sawError);
+    });
+
+});
+
+describe('Update student in registry', function() {
+
+    let stud1 = {
+        name: "Johnny Brown", 
+        entered: 2010, grade: 1,
+        gender: "male"
+    };
+    let studentid1;
+
+    before(async function() {
+        studentid1 = await registrar.createStudent(stud1);
+    });
+
+
+    it('should update student', async function() {
+        await registrar.updateStudent(studentid1, {
+            gender: "female"
+        });
+        let student = await registrar.getStudent(studentid1);
+
+        assert.exists(student);
+        assert.isObject(student);
+        assert.isString(student.name);
+        assert.equal(student.name, stud1.name);
+        assert.isNumber(student.entered);
+        assert.equal(student.entered, stud1.entered);
+        assert.isNumber(student.grade);
+        assert.equal(student.grade, stud1.grade);
+        assert.isString(student.gender);
+        assert.equal(student.gender, "female");
+    });
+
+    it('should fail to update student with bad data', async function() {
+        let caughtError = false;
+        try {
+            await registrar.updateStudent(studentid1, {
+                entered: "female"
+            });
+        } catch (e) {
+            // console.log(e);
+            caughtError = true;
+        }
+        assert.isTrue(caughtError);
+
+        let student = await registrar.getStudent(studentid1);
+
+        assert.exists(student);
+        assert.isObject(student);
+        assert.isString(student.name);
+        assert.equal(student.name, stud1.name);
+        assert.isNumber(student.entered);
+        assert.equal(student.entered, stud1.entered);
+        assert.isNumber(student.grade);
+        assert.equal(student.grade, stud1.grade);
+        assert.isString(student.gender);
+        assert.equal(student.gender, "female");
+    });
+});
+
+describe('Delete student from registry', function() {
+
+    let stud1 = {
+        name: "JohnTODELETE Brown", 
+        entered: 2010, grade: 1,
+        gender: "male"
+    };
+    let studentid1;
+
+    before(async function() {
+        studentid1 = await registrar.createStudent(stud1);
+    });
+
+    it('should not fail to delete student using bad ID', async function() {
+
+        let caughtError = false;
+        try {
+            await registrar.deleteStudent(9999999999);
+        } catch (e) {
+            // console.log(e);
+            caughtError = true;
+        }
+        assert.isFalse(caughtError);
+    });
+
+    it('should delete student using good ID', async function() {
+        await registrar.deleteStudent(studentid1);
+        let student;
+        let caughtError = false;
+        try {
+            student = await registrar.getStudent(studentid1);
+        } catch (e) {
+            caughtError = true;
+        }
+        assert.isTrue(caughtError);
     });
 });
