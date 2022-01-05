60aa4534a0ee8c6cfe3bb3e3d94179edd9ebb639
David Herron <david@davidherron.com>
2022-01-04 16:53:37 -0800

    Add tabular output

--- registrar-cli/src/commands/students/index.ts
+++ registrar-cli/src/commands/students/index.ts
@@ -1,15 +1,36 @@
 import {Command, Flags} from '@oclif/core';
-import cli from 'cli-ux';
+import Table = require('cli-table');
 
 import * as Registrar from 'registrar';
 
 export default class Students extends Command {
     static description = 'List students';
 
+    static flags = {
+        table: Flags.boolean({
+            description: 'Output in table format',
+            required: false
+        })
+    };
+
     async run(): Promise<void> {
+        const {args, flags} = await this.parse(Students);
         await Registrar.connect("registrardb.sqlite");
         const userlist = await Registrar.getStudentRepository()
                         .findAll();
-        console.log(userlist);
+        if (flags.table) {
+            let table = new Table({
+                head: ['ID', 'Name', 'Entered', 'Year', 'Gender']
+            });
+            for (let student of userlist) {
+                table.push([
+                    student.id, student.name, student.entered,
+                    student.grade, student.gender
+                ]);
+            }
+            console.log(table.toString());
+        } else {
+            console.log(userlist);
+        }
     }
 }
