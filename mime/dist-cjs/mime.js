5fb3a2fa370cf62e078fcf5ce4aa2f50462244ee
David Herron <david@davidherron.com>
2022-01-23 13:27:14 -0800

    MIME example

---
+++ mime/dist-cjs/mime.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mime = require("mime");
+console.log(mime.getType('foo.json'));
---
+++ mime/mime.ts
@@ -0,0 +1,4 @@
+
+import * as mime from 'mime';
+
+console.log(mime.getType('foo.json'));
